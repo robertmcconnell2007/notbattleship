55b6f9efec96172f54be6b0bf584691b9b77ae25
ignatusfordon <ignatusfordon@8053601f-75d8-cd8d-607e-0a55a0aef1d8>
2010-09-28 02:27:21 +0000

    added basic structure for game. not ready to begin coding yet.

---
+++ trunk/src/game/Game.java
@@ -0,0 +1,10 @@
+package game;
+/*
+ * Game will create game boards in order to play
+ * if playing single-player, game will create 2 GameBoards
+ * if playing multi-player, game will set up networking
+ */
+public class Game
+{
+
+}
